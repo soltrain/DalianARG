092535a93d8c29197122aa47467c06503192c0aa
Noah Solnick <soltrain@berkeley.edu>
2013-05-16 02:01:24 +0800

    changes going to EC2

--- text.sh
+++ text.sh
@@ -2,5 +2,5 @@
 ACCOUNTSID=AC6d1e6f7497f9f28b227762c9c4656280
 AUTHTOKEN=479b4c1e185f75db874e83035f9ba470
 CALLERID=415-749-9876
-sleep 180
-curl -fSs -u "$ACCOUNTSID:$AUTHTOKEN" -d "From=$CALLERID" -d "To=%2B86$1" -d "Body=final test message2" "https://api.twilio.com/2010-04-01/Accounts/$ACCOUNTSID/SMS/Messages" 2>&1
+sleep 200
+curl -fSs -u "$ACCOUNTSID:$AUTHTOKEN" -d "From=$CALLERID" -d "To=%2B86$1" -d "Body=Thank you for helping me. I know I can trust you. Send me an email at SARA@contactdalian.com and I will contact you after I gather more information." "https://api.twilio.com/2010-04-01/Accounts/$ACCOUNTSID/SMS/Messages" 2>&1
